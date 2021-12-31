29f9a7c0de1baa7a4ef2e43f5e8be5dc9b8e10d7
daehocho <daehocho0637@gmail.com>
2021-12-31 17:11:02 +0900

    searchBar 구현 첫번째

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    
+//    searchBar에 검색어를 쓰고 search버튼을 눌렀을때 이벤트
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // 검색시작
+        print("검색어 : \(searchBar.text)")
+    }
 }
